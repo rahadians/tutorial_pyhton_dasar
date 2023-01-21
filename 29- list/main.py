ebd76ec0c1723a7bd92e10d6a8460bb616e733f5
Rahadians <matrix.black82@gmail.com>
2023-01-21 17:10:09 +0700

    helajae

--- 29- list/main.py
+++ 29- list/main.py
@@ -1,11 +1,25 @@
+# ---LIST---
+
+# kumpulan data numbers
+
 data_angka=[1,2,3]
 print(data_angka)
 
 data_range=range(0,10)
 print(data_range)
 
-data
 
+#kumpulan data string
+data_string =["ucup","otorng","odah"]
+print(data_string)
+
+
+data_boolean =[True,False,True,False]
+print(data_boolean)
+
+
+data_campuran=[1,True,"Bambang"]
+print(data_campuran)
 
 
 
